2efa991d603caee9236ebc20a3be7d6e9bd59851
pavimann5 <pavimann5@gmail.com>
2026-07-21 14:23:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -80,3 +80,27 @@ document.querySelectorAll("nav a").forEach(link => {
         nav.classList.remove("active");
     });
 });
+/ ===== LIGHTBOX =====
+
+const galleryImages = document.querySelectorAll(".gallery-item img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+galleryImages.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+        lightboxImg.alt = img.alt;
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = "none";
+    }
+});
