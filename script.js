167b8b1c65a454cd2f49a6a8154778571b96c881
pavimann5 <pavimann5@gmail.com>
2026-07-21 16:14:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -89,23 +89,26 @@ const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const closeBtn = document.querySelector(".close");
 
-galleryImages.forEach(img => {
-    img.addEventListener("click", () => {
-        lightbox.style.display = "flex";
-        lightboxImg.src = img.src;
-        lightboxImg.alt = img.alt;
+if (galleryImages.length > 0 && lightbox && lightboxImg && closeBtn) {
+
+    galleryImages.forEach(img => {
+        img.addEventListener("click", () => {
+            lightbox.style.display = "flex";
+            lightboxImg.src = img.src;
+            lightboxImg.alt = img.alt;
+        });
     });
-});
 
-closeBtn.addEventListener("click", () => {
-    lightbox.style.display = "none";
-});
-
-lightbox.addEventListener("click", (e) => {
-    if (e.target === lightbox) {
+    closeBtn.addEventListener("click", () => {
         lightbox.style.display = "none";
-    }
-});
+    });
+
+    lightbox.addEventListener("click", (e) => {
+        if (e.target === lightbox) {
+            lightbox.style.display = "none";
+        }
+    });
+}
 // Auto Copyright Year
 const year = document.getElementById("year");
 
