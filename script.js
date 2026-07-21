1db0e154aa2fe78e00c319909cffffebf6bbe4bc
pavimann5 <pavimann5@gmail.com>
2026-07-21 13:30:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,3 +65,11 @@ if (topBtn) {
 }
 
 console.log("Alfaz Official Website Loaded Successfully.");
+// ===== MOBILE MENU =====
+
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
